883e7688d4b2d5eff5b27094f4a5c8c3a788a9e2
hulinglong <250717586@qq.com>
2026-06-10 18:35:03 +0800

    增加vconsole依赖

--- src/main.js
+++ src/main.js
@@ -4,4 +4,11 @@ import "vant/lib/index.css";
 import App from "./App.vue";
 import "./styles.css";
 
+// 开发和测试环境启用 vConsole 调试
+if (import.meta.env.VITE_ENV === 'development' || import.meta.env.VITE_ENV === 'staging') {
+  import('vconsole').then(module => {
+    new module.default();
+  });
+}
+
 createApp(App).use(Vant).mount("#app");
