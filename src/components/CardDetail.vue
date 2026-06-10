8fb87729c21aac0f79369bdf74e9a83b5f1a2c40
luoyou <1049789273@qq.com>
2026-06-10 19:10:40 +0800

    路径修改

--- src/components/CardDetail.vue
+++ src/components/CardDetail.vue
@@ -147,16 +147,16 @@ function handleJumpToMiniProgram() {
     if (isMiniProgram) {
       //showToast({ message: 'path=' + path , duration: 2000 });
       // 场景1：小程序内 webview → 跳转同小程序的页面
-      wx.miniProgram.navigateTo({ url: path, success: s => {
+      wx.miniProgram.navigateTo({ url: '/' + path, success: s => {
           console.log(">>>>> 跳转成功：", s)
         }, fail: e => {
           console.log(">>>>> 跳转错误：", e)
         }, complete: o => {
           console.log(">>>>> 跳转结果1：", o)
         } });
-      wx.miniProgram.navigateTo({ url: '/pages/selectScenery/Index', complete: o => {
-        console.log(">>>>> 跳转结果2：", o)
-        } });
+      // wx.miniProgram.navigateTo({ url: '/pages/selectScenery/Index', complete: o => {
+      //   console.log(">>>>> 跳转结果2：", o)
+      //   } });
     } else if (isWechat) {
       // 场景2：微信内置浏览器 → 通过 URL Scheme 打开小程序
       const encodedPath = encodeURIComponent(path);
